e4e961ee1b2ecae3cb909ed1cb3c6c7eecdeeb48
owenmatejka <69021860+owenmatejka@users.noreply.github.com>
2022-02-13 10:33:21 -0800

    Update database.js

--- functions/generic/database.js
+++ functions/generic/database.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Database Connected!");
 })
 
 module.exports = connection;
