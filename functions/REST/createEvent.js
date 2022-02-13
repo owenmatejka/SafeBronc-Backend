716fb0379f2effa19ebb5bea18acff45b21fc589
Byron Cuachin <byroncuachin@gmail.com>
2022-02-12 23:09:23 -0800

    for loop and comments

--- functions/REST/createEvent.js
+++ functions/REST/createEvent.js
@@ -1,15 +1,26 @@
 const sql = require("../generic/database.js");
 
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes()+5 + ":" + today.getSeconds();
-var dateTime = date+' '+time;
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + 5 + ":" + today.getSeconds();
+var dateTime = date + ' ' + time;
 
 module.exports = (eventName, eventLocation, PhoneNbrs) => {
     sql.query(`INSERT INTO events(EventName,Location,ExpireTime,PhoneNbrs) 
     VALUES(${eventName},${eventLocation},${dateTime},${""})`)
+
     //for nbr in phoneNbrs
-    
+    for (let i = 0; i < PhoneNbrs.length; i++) {
+        sql.query(`INSERT INTO contacts(EventName,PhoneNbrs,isVerified) 
+        VALUES(${eventName},${PhoneNbrs[i]}), ${false})`)
+    }
+
     //    send text
+    //    event listener
+    //    get phone number and set isVerified to true
+    let phoneNbrs = 0;
+    sql.query(`SELECT * FROM contacts WHERE PhoneNbr = ${phoneNbr}`)
+    // add contactId to event(ContactIds)
+
     return {}
 }
--- safebronc.session.sql
+++ safebronc.session.sql
@@ -2,6 +2,7 @@ CREATE TABLE safebronc.user{
     Personid int NOT NULL AUTO_INCREMENT,
     LastName varchar(255) NOT null,
     FirstName varchar(255),
+    isVerified BIT(1) DEFAULT 0,
     PRIMARY key(Personid)
 }
 
